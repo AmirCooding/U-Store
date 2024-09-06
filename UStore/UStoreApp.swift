40460b63565784e81127a3fee2e12f5bd3083d5e
Amir <amirrllotfi@gmail.com>
2024-09-06 12:36:14 +0200

    Initial Commit

---
+++ UStore/UStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  UStoreApp.swift
+//  UStore
+//
+//  Created by Amir Lotfi on 06.09.24.
+//
+
+import SwiftUI
+
+@main
+struct UStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
